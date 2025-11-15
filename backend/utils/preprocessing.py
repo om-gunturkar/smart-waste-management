53ce157952545cd708866eac7836406f8227b82f
Om Gunturkar <omgunturkar@gmail.com>
2025-11-16 02:48:13 +0530

    Clean backend for Render deployment

--- backend/utils/preprocessing.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-from PIL import Image
-
-def preprocess_image(image_bytes):
-    image = Image.open(image_bytes).convert('RGB')
-    image = image.resize((224, 224))
-    img_array = np.array(image) / 255.0
-    return np.expand_dims(img_array, axis=0)
